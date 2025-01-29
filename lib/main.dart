0c409f033158d9d9bd394ad3093c6965747f9c63
Dinidu Sehara <dinidusehara@gmail.com>
2025-01-29 19:54:36 +0530

    Remove unnecessary codes

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@ import 'package:assingment/Screens/AboutUs.dart';
 import 'package:assingment/Screens/Fragrances.dart';
 import 'package:assingment/Screens/HomePage.dart';
 import 'package:assingment/Screens/LeatherGoods.dart';
-import 'package:assingment/Screens/Footer.dart';
-import 'package:assingment/Screens/Accesories.dart';
+import 'package:assingment/Screens/bottomNav.dart';
+import 'package:assingment/Screens/accesories.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,108 +12,14 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color.fromARGB(255, 250, 243, 243)),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Aurora Luxe'),
+      theme:
+          ThemeData(useMaterial3: true, scaffoldBackgroundColor: Colors.white),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-            title: Text(widget.title),
-            centerTitle: true,
-            actions: [
-              IconButton(
-                icon: Icon(Icons.shopping_cart),
-                onPressed: () {},
-              )
-            ]),
-        drawer: Drawer(
-          child: ListView(
-            children: [
-              ListTile(
-                // leading: Icon(Icons.home),
-                title: Text("Home"),
-                hoverColor: Colors.grey,
-                onTap: () {
-                  Navigator.pop(context);
-                },
-              ),
-              ListTile(
-                title: Text("Leather goods"),
-                hoverColor: Colors.grey,
-                onTap: () {
-                  Navigator.pushReplacement(context,
-                      MaterialPageRoute(builder: (context) => LeatherGoods()));
-                },
-              ),
-              ListTile(
-                title: Text("Fragrances"),
-                hoverColor: Colors.grey,
-                onTap: () {
-                  Navigator.pushReplacement(context,
-                      MaterialPageRoute(builder: (context) => Fragrances()));
-                },
-              ),
-              ListTile(
-                title: Text("Accessories"),
-                hoverColor: Colors.grey,
-                onTap: () {
-                  Navigator.pushReplacement(context,
-                      MaterialPageRoute(builder: (context) => Accesories()));
-                },
-              ),
-              ListTile(
-                title: Text("About Us"),
-                hoverColor: Colors.grey,
-                onTap: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => Aboutus()));
-                },
-              ),
-              const Divider(),
-              ListTile(
-                leading: Icon(Icons.login),
-                title: Text("Login"),
-                hoverColor: Colors.grey,
-                onTap: () {},
-              )
-            ],
-          ),
-        ),
-        body: Column(
-          children: [MainImg(), NewArrivals(), Footer()],
-        ));
-  }
-}
