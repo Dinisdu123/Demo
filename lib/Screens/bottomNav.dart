a90737cae12ef98ff50aafd8d0b6c9b17e098ae2
Dinidu Sehara <dinidusehara@gmail.com>
2025-01-29 18:18:45 +0530

    Link profile

---
+++ lib/Screens/bottomNav.dart
@@ -0,0 +1,37 @@
+import 'package:assingment/Screens/Profile.dart';
+import 'package:assingment/Screens/Shop.dart';
+import 'package:flutter/material.dart';
+import 'package:assingment/Screens/HomePage.dart';
+
+class Footer extends StatelessWidget {
+  const Footer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationBar(
+      onDestinationSelected: (index) {
+        if (index == 0) {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => MyHomePage()),
+          );
+        } else if (index == 1) {
+          Navigator.pushReplacement(
+              context, MaterialPageRoute(builder: (context) => Search()));
+        } else if (index == 2) {
+          Navigator.pushReplacement(
+              context, MaterialPageRoute(builder: (context) => Search()));
+        } else if (index == 3) {
+          Navigator.pushReplacement(
+              context, MaterialPageRoute(builder: (context) => MyProfile()));
+        }
+      },
+      destinations: const [
+        NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+        NavigationDestination(icon: Icon(Icons.shop), label: 'Shop'),
+        NavigationDestination(icon: Icon(Icons.shopping_cart), label: 'Cart'),
+        NavigationDestination(icon: Icon(Icons.person), label: 'Profile'),
+      ],
+    );
+  }
+}
