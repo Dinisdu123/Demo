7aad5fbb483c14ac727b479fe98f91634d2b0ca9
Dinidu Sehara <dinidusehara@gmail.com>
2025-01-30 10:58:31 +0530

    Dev themes

---
+++ lib/Themes/Themes.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+  brightness: Brightness.light,
+  colorScheme: ColorScheme.light(
+    onSurface: Colors.black,
+  ),
+  textTheme: TextTheme(
+    bodyLarge: TextStyle(color: Colors.black),
+    bodyMedium: TextStyle(color: Colors.black),
+  ),
+  iconTheme: IconThemeData(
+    color: Colors.black,
+  ),
+);
+
+ThemeData darkMode = ThemeData(
+  brightness: Brightness.dark,
+  colorScheme: ColorScheme.dark(
+    onSurface: Colors.white,
+  ),
+  textTheme: TextTheme(
+    bodyLarge: TextStyle(color: Colors.white),
+    bodyMedium: TextStyle(color: Colors.white),
+  ),
+  iconTheme: IconThemeData(
+    color: Colors.white,
+  ),
+);
