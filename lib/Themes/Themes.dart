374ee07d67d98bd3305241104d340ec2223e8911
Dinidu Sehara <dinidusehara@gmail.com>
2025-01-31 11:36:23 +0530

    add font family

--- lib/Themes/Themes.dart
+++ lib/Themes/Themes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 ThemeData lightMode = ThemeData(
+  fontFamily: 'Roboto',
   brightness: Brightness.light,
   colorScheme: ColorScheme.light(
     onSurface: Colors.black,
@@ -15,6 +16,7 @@ ThemeData lightMode = ThemeData(
 );
 
 ThemeData darkMode = ThemeData(
+  fontFamily: 'Roboto',
   brightness: Brightness.dark,
   colorScheme: ColorScheme.dark(
     onSurface: Colors.white,
