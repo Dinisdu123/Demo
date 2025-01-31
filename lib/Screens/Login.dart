dc82e86e8731c46912cfd970cff2700e8eedef5d
Dinidu Sehara <dinidusehara@gmail.com>
2025-01-31 10:07:56 +0530

    Login page developed

--- lib/Screens/Login.dart
+++ lib/Screens/Login.dart
@@ -1,4 +1,5 @@
-import 'package:assingment/Screens/bottomNav.dart';
+import 'package:assingment/Screens/HomePage.dart';
+import 'package:assingment/Screens/Register.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatelessWidget {
@@ -7,7 +8,47 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: Footer(),
+      appBar: AppBar(
+        title: Text("Aurora Luxe - Login"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 30, left: 20, right: 20),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                  labelText: "Email", border: OutlineInputBorder()),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                  labelText: "Password", border: OutlineInputBorder()),
+            ),
+            TextButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => Register()));
+                },
+                child: Text(
+                  "Don't have Account? Register Now",
+                  style: TextStyle(
+                      color: Theme.of(context).textTheme.bodyMedium?.color),
+                )),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (context) => MyHomePage()));
+                },
+                child: Text(
+                  "Login",
+                  style: TextStyle(
+                      color: Theme.of(context).textTheme.bodyMedium?.color),
+                )),
+          ],
+        ),
+      ),
     );
   }
 }
