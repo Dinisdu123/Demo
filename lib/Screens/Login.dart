032c799180af2859675e8107465d2e4ecdbd06aa
Dinidu Sehara <dinidusehara@gmail.com>
2025-01-31 11:18:28 +0530

    add page animation

--- lib/Screens/Login.dart
+++ lib/Screens/Login.dart
@@ -1,9 +1,28 @@
-import 'package:assingment/Screens/HomePage.dart';
 import 'package:assingment/Screens/Register.dart';
+
+import 'package:assingment/Screens/HomePage.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatelessWidget {
   const Login({super.key});
+
+  Route _createRoute(Widget page) {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) => page,
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        const begin = Offset(1.0, 0.0);
+        const end = Offset.zero;
+        const curve = Curves.easeInOut;
+
+        var tween =
+            Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+        var offsetAnimation = animation.drive(tween);
+
+        return SlideTransition(position: offsetAnimation, child: child);
+      },
+      transitionDuration: Duration(seconds: 1),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,8 +47,10 @@ class Login extends StatelessWidget {
             ),
             TextButton(
                 onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => Register()));
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Register()),
+                  );
                 },
                 child: Text(
                   "Don't have Account? Register Now",
@@ -38,8 +59,10 @@ class Login extends StatelessWidget {
                 )),
             ElevatedButton(
                 onPressed: () {
-                  Navigator.pushReplacement(context,
-                      MaterialPageRoute(builder: (context) => MyHomePage()));
+                  Navigator.pushReplacement(
+                    context,
+                    _createRoute(MyHomePage()),
+                  );
                 },
                 child: Text(
                   "Login",
